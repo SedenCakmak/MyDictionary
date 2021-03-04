613d7fb1254ed664d3946c2611feb41934110c72
Seden Nur Çakmak <seden.cakmak@mf.karaelmas.edu.tr>
2021-03-04 20:47:01 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Dictionary<string> isimler = new Dictionary<string>();
+
+                isimler.Add("Seden");
+                Console.WriteLine(isimler.Length);
+
+                isimler.Add("Seymen");
+                Console.WriteLine(isimler.Length);
+
+                foreach (var isim in isimler.Items)
+                {
+                    Console.WriteLine(isim);
+                }
+
+
+            }
+
+        }
+    }
+}
